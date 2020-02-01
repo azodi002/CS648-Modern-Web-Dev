8c8a26634746ce7c9c8bf4c2848e0ad18914d0b6
Omid Azodi <azodi002@users.noreply.github.com>
2020-02-01 14:40:43 -0800

    Assignment 1 upload

---
+++ Assignment1/public/App.js
@@ -0,0 +1,23 @@
+"use strict";
+
+var message = "Omid Azodi";
+var nameElement = React.createElement("div", {
+  title: "Outer div"
+}, React.createElement("h1", {
+  class: "name"
+}, message));
+var button_element = React.createElement("button", {
+  type: "button",
+  class: "repo"
+}, "View My GitHub Repo");
+var introduction_element = React.createElement("p", {
+  class: "intro"
+}, "I am in my last year of my Master's program here at SDSU, studying Computer Science. I currently work at BAE Systems as a Software Engineer. In my free time I enjoy playing my PS4 and watching Football & Basketball.");
+var picture_element = React.createElement("picture", null, React.createElement("img", {
+  class: "omid_pic",
+  src: "Omid.png"
+}));
+ReactDOM.render(nameElement, document.getElementById('content'));
+ReactDOM.render(introduction_element, document.getElementById('introduction_content'));
+ReactDOM.render(picture_element, document.getElementById('picture_content'));
+ReactDOM.render(button_element, document.getElementById('button_content'));
---
+++ Assignment1/src/App.jsx
@@ -0,0 +1,28 @@
+const message = "Omid Azodi"
+
+const nameElement = (
+  <div title="Outer div">
+    <h1 class="name">{message}</h1>
+  </div>
+);
+
+const button_element = (
+	<button type="button" class = "repo">
+		View My GitHub Repo
+	</button>);
+
+const introduction_element = (
+	<p class="intro">
+		I am in my last year of my Master's program here at SDSU, studying Computer Science. I currently work at BAE Systems as a Software Engineer.
+		In my free time I enjoy playing my PS4 and watching Football & Basketball. 
+	</p>);
+
+const picture_element = (
+	<picture>
+		<img class="omid_pic" src="Omid.png"/>
+	</picture>);
+
+ReactDOM.render(nameElement, document.getElementById('content'));
+ReactDOM.render(introduction_element, document.getElementById('introduction_content'));
+ReactDOM.render(picture_element, document.getElementById('picture_content'));
+ReactDOM.render(button_element, document.getElementById('button_content'));
